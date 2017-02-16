4f2b167e6caa24366c81ed023f19660ba2f6c4f6
deekhalden <iliy4@ua.fm>
2017-02-16 18:02:39 +0200

    news page fully done

--- src/assets/scripts/client/dropdown.js
+++ src/assets/scripts/client/dropdown.js
@@ -1,14 +1,25 @@
 $('.dropdown .item').on('click',function() {
-        
         $(this).siblings().children().removeClass('links-wrapper--block');
         $(this).children().toggleClass('links-wrapper--block');
 });
 
+$('.dropdown .item').hover(function(){
+    var item = $(this);
+    var a = setTimeout(function(){
+        item.siblings().children().removeClass('links-wrapper--block');
+        item.children().toggleClass('links-wrapper--block');
+        
+    },500);
+});
+
+
+
 $('.filter .close-btn').on('click',function() {
-    console.log($(this).parent());
+    
     $(this).parent().css('visibility','hidden');
 });
 
 $('.filter-menu .item').on('click',function() {
+
     $('.filter-dropdown').css('visibility', 'visible');
 })
