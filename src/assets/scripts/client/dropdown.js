bf8ca558aeb831c7d35130f3ac449226e239e454
deekhalden <iliy4@ua.fm>
2017-02-22 18:03:27 +0200

    fixes from test list

--- src/assets/scripts/client/dropdown.js
+++ src/assets/scripts/client/dropdown.js
@@ -1,16 +1,18 @@
 $('.dropdown .item').on('click',function() {
+
         $(this).siblings().children().removeClass('links-wrapper--block');
-        $(this).children().toggleClass('links-wrapper--block');
+        $(this).toggleClass('item--active');
+        $(this).find('ul').toggleClass('links-wrapper--block');
 });
 
-$('.dropdown .item').hover(function(){
-    var item = $(this);
-    var a = setTimeout(function(){
-        item.siblings().children().removeClass('links-wrapper--block');
-        item.children().toggleClass('links-wrapper--block');
+// $('.dropdown .item').hover(function(){
+//     var item = $(this);
+//     var a = setTimeout(function(){
+//         item.siblings().children().removeClass('links-wrapper--block');
+//         item.children().toggleClass('links-wrapper--block');
         
-    },500);
-});
+//     },500);
+// });
 
 
 
@@ -19,7 +21,11 @@ $('.filter .close-btn').on('click',function() {
     $(this).parent().css('visibility','hidden');
 });
 
-$('.filter-menu .item').on('click',function() {
-
+$('.filter-menu .item').hover(function() {
+    var a = setTimeout(function(){
+        $('.filter-dropdown').css('visibility', 'visible');
+    },500);
+});
+$('.filter-menu .item').on('click hover',function() {
     $('.filter-dropdown').css('visibility', 'visible');
-})
+});
